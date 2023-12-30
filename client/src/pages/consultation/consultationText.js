0ba78a5e9e027218f9723960f8b12886bf92a497
Ankit Tomar <ankitjdec@gmail.com>
2023-12-30 20:59:38 +0530

    added the consultation part

--- client/src/pages/consultation/consultationText.js
+++ client/src/pages/consultation/consultationText.js
@@ -6,9 +6,25 @@ import Splash from "../../components/splash/splash";
 const Consultation = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
+  const [doubt, setDoubt] = useState("");
+  const [question, setQuestion] = useState([]);
+  const [response, setResponse] = useState("");
   setTimeout(() => {
     setLoading(false);
   }, 2000);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (doubt.trim() !== "") {
+      setQuestion([...question, doubt.trim()]);
+      setDoubt("");
+    }
+    setTimeout(() => {
+      setResponse(
+        "Thank you for reaching out! We appreciate your inquiry. Our team will review your request, and we'll get back to you as soon as possible. Your patience is highly valued. If you have any further questions or urgent matters, feel free to contact us directly. We look forward to assisting you!"
+      );
+    }, [3000]);
+  };
+
   return (
     <HomeLayout>
       {loading ? (
@@ -16,13 +32,127 @@ const Consultation = () => {
       ) : (
         <section
           style={{
-            padding: "60px 24px 100px 24px",
+            padding: "80px 24px 100px 24px",
             backgroundColor: "#f3f4f6",
             height: "auto",
             width: "100%",
           }}
         >
-          Consultation Page Text
+          <div
+            style={{
+              width: "100%",
+            }}
+          >
+            {question.map((q, index) => (
+              <div key={index}>
+                <p
+                  style={{
+                    position: "relative",
+                    padding: "12px",
+                    backgroundColor: "#fff",
+                    borderRadius: "4px",
+                    margin: "12px 0",
+                    overflowWrap: "break-word",
+                    fontSize: "20px",
+                    fontWeight: "500",
+                  }}
+                >
+                  {q}
+                  <span
+                    style={{
+                      position: "absolute",
+                      bottom: 0,
+                      right: 0,
+                      width: 0,
+                      height: 0,
+                      borderTop: "10px solid transparent",
+                      borderLeft: "10px solid transparent",
+                      borderBottom: "10px solid #07e9a1",
+                      borderRight: "10px solid #07e9a1",
+                    }}
+                  ></span>
+                </p>
+                <p
+                  style={{
+                    position: "relative",
+                    padding: "12px",
+                    backgroundColor: "#a9cdf9",
+                    borderRadius: "4px",
+                    margin: "12px 0",
+                    overflowWrap: "break-word",
+                    fontSize: "18px",
+                    fontWeight: "400",
+                    color: "#fff",
+                    letterSpacing: "1px",
+                    lineHeight: "24px",
+                    textAlign: "center",
+                  }}
+                  key={index}
+                >
+                  {" "}
+                  {response}
+                  <span
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: 0,
+                      height: 0,
+                      borderTop: "10px solid transparent",
+                      borderLeft: "10px solid transparent",
+                      borderBottom: "10px solid #ffd66d",
+                      borderRight: "10px solid #ffd66d",
+                      rotate: "180deg",
+                    }}
+                  ></span>
+                </p>
+              </div>
+            ))}
+          </div>
+          <form onSubmit={handelSubmit}>
+            <div
+              style={{
+                position: "fixed",
+                bottom: 80,
+                left: 0,
+                width: "100%",
+                padding: " 8px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <input
+                style={{
+                  width: "100%",
+                  padding: "24px",
+                  borderRadius: "8px",
+                  border: "0",
+                  boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+                }}
+                onFocus={(e) => (e.target.style.outline = "2px solid #007bff")}
+                onBlur={(e) => (e.target.style.outline = "none")}
+                type="text"
+                placeholder="Type Your Doubt...."
+                onChange={(e) => {
+                  setDoubt(e.target.value);
+                }}
+                value={doubt}
+              />
+              <button
+                type="submit"
+                style={{
+                  backgroundColor: "#f76152",
+                  padding: "22px 4px",
+                  borderRadius: "4px",
+                  fontWeight: "500",
+                }}
+              >
+                Ask?
+              </button>
+            </div>
+          </form>
         </section>
       )}
     </HomeLayout>
--- client/src/pages/consultation/consultationVideo.js
+++ client/src/pages/consultation/consultationVideo.js
@@ -6,9 +6,18 @@ import Splash from "../../components/splash/splash";
 const Consultation = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
+  const [Contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [additional, setAdditional] = useState("");
+  const [question, setQuestion] = useState([]);
   setTimeout(() => {
     setLoading(false);
   }, 2000);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.warn(email, Contact, date, additional);
+  };
   return (
     <HomeLayout>
       {loading ? (
@@ -22,7 +31,134 @@ const Consultation = () => {
             width: "100%",
           }}
         >
-          Consultation Page Vide
+          <form onSubmit={handelSubmit}>
+            <div style={{ marginTop: "24px" }}>
+              <p style={{ textAlign: "left", fontFamily: "Bungee" }}>
+                Enter Email:-
+              </p>
+              <input
+                style={{
+                  marginTop: "12px",
+                  width: "100%",
+                  padding: "20px",
+                  borderRadius: "8px",
+                  border: "0",
+                  boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+                  fontSize: "22px",
+                  fontWeight: "500",
+                }}
+                onFocus={(e) => (e.target.style.outline = "2px solid #007bff")}
+                onBlur={(e) => (e.target.style.outline = "none")}
+                type="email"
+                placeholder="eg ...abc@gmail.com"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                value={email}
+              />
+            </div>
+            <div style={{ marginTop: "24px" }}>
+              <p style={{ textAlign: "left", fontFamily: "Bungee" }}>
+                Enter Contact Number:-
+              </p>
+              <input
+                style={{
+                  marginTop: "12px",
+                  width: "100%",
+                  padding: "20px",
+                  borderRadius: "8px",
+                  border: "0",
+                  boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+                  fontSize: "22px",
+                  fontWeight: "500",
+                }}
+                onFocus={(e) => (e.target.style.outline = "2px solid #007bff")}
+                onBlur={(e) => (e.target.style.outline = "none")}
+                type="number"
+                placeholder="eg..1444-4569-5896"
+                onChange={(e) => {
+                  setContact(e.target.value);
+                }}
+                value={Contact}
+              />
+            </div>
+            <div style={{ marginTop: "24px" }}>
+              <p style={{ textAlign: "left", fontFamily: "Bungee" }}>
+                Select Date:-
+              </p>
+              <input
+                style={{
+                  marginTop: "12px",
+                  width: "100%",
+                  padding: "20px",
+                  borderRadius: "8px",
+                  border: "0",
+                  boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+                  fontSize: "22px",
+                  fontWeight: "500",
+                }}
+                onFocus={(e) => (e.target.style.outline = "2px solid #007bff")}
+                onBlur={(e) => (e.target.style.outline = "none")}
+                type="date"
+                placeholder="eg ...abc@gmail.com"
+                onChange={(e) => {
+                  setDate(e.target.value);
+                }}
+                value={date}
+              />
+            </div>
+            <div style={{ marginTop: "24px" }}>
+              <p style={{ textAlign: "left", fontFamily: "Bungee" }}>
+                Any Additional Information:-
+              </p>
+              <textarea
+                style={{
+                  marginTop: "12px",
+                  width: "100%",
+                  padding: "20px",
+                  borderRadius: "8px",
+                  border: "0",
+                  boxShadow: "0px -2px 10px rgba(0, 0, 0, 0.1)",
+                  height: "200px",
+                  width: "100%",
+                  fontSize: "22px",
+                  fontWeight: "500",
+                }}
+                onFocus={(e) => (e.target.style.outline = "2px solid #007bff")}
+                onBlur={(e) => (e.target.style.outline = "none")}
+                type="email"
+                placeholder=""
+                onChange={(e) => {
+                  setAdditional(e.target.value);
+                }}
+                value={additional}
+              />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: "18px",
+                }}
+              >
+                <input
+                  style={{
+                    width: "70%",
+                    padding: "14px 24px",
+                    borderRadius: "8px",
+                    backgroundColor: "#f76152",
+                    fontSize: "22px",
+                    fontWeight: "600",
+                    color: "#fff",
+                    border: "0",
+                    opacity: "0.5",
+                  }}
+                  type="submit"
+                  value="Save Changes"
+                />
+              </div>
+            </div>
+          </form>
         </section>
       )}
     </HomeLayout>

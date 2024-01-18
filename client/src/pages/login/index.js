5d19c91b17cc6039ba74f9b5f85b4d17ec5a4278
Ankit Tomar <ankitjdec@gmail.com>
2024-01-18 22:25:43 +0530

    setted the basic server for the registration preoces

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,194 +1,33 @@
-import React, { useEffect, useState } from "react";
-import "./styles.css";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from "../../components/firebase/firebase"
+import HomeLayout from "../../layout/HomeLayout/HomeLayout";
+import Splash from "../../components/splash/splash";
 
-function Login() {
-
-  const [login, setLogin] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+const Login = () => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    // Check if the user is already logged in
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        
-        // You can use React Router or another navigation method here
-        console.log('User is logged in:', user);
-      } else {
-        // No user is logged in
-        console.log('No user is logged in');
-      }
-    });
-
-    // Clean up the subscription when the component unmounts
-    return () => unsubscribe();
-  }, []);
-
-  const handleSubmit = async(e) => {
-
-    e.preventDefault();
-    if (email && password) {
-      try {
-        if (login) {
-          // User is trying to log in
-          await signInWithEmailAndPassword(auth, email, password);
-          setEmail('');
-          setPassword('');
-          navigate("/home");
-        } else {
-          // User is trying to sign up
-          await createUserWithEmailAndPassword(auth, email, password);
-          if(!login){
-            setEmail("")
-            setPassword("")
-            setLogin(!login)
-          }
-        }
-
-        // Clear the input fields
-        // Redirect to the home page or another page upon successful login/signup
-        // You can use React Router or another navigation method here
-        console.log('Successful login/signup');
-      } catch (error) {
-        // Handle login/signup errors
-        console.error('Error:', error.message);
-        alert('invalid user');
-      }
-    } else {
-      alert('Fill in all data');
-      setError(true);
-    }
-  };
-
-  const handleSwitch = () => {
-    setEmail("");
-    setPassword("");
-    setLogin(!login);
-  };
-
+  const [loading, setLoading] = useState(false);
+  // setTimeout(() => {
+  //   setLoading(false);
+  // }, 2000);
   return (
-    <div className="container">
-      <div className="left">
-        <div className="login-section">
-          <header>
-            <h2
-              className="animation a1"
-              style={{
-                fontFamily: "Space Grotesk",
-                color: "#171717",
-                fontSize: "52px",
-                fontWeight: "700",
-              }}
-            >
-              Aupdeshik
-            </h2>
-            <h4
-              className="animation a2"
-              style={{
-                color: "#171717",
-                opacity: "0.5",
-                lineHeight: "22px",
-                fontSize: "16px",
-              }}
-            >
-              {login
-                ? " Welcome back, Please login to your account"
-                : " Join our platform 😊"}
-            </h4>
-          </header>
-          <form method="POST">
-            <input
-              type="email"
-              placeholder="Email"
-              className="input-field animation a3"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            {error && email === "" && (
-              <p
-                style={{
-                  lineHeight: "16px",
-                  marginTop: "4px",
-                  color: "red",
-                  fontSize: "12px",
-                }}
-              >
-                Email is required
-              </p>
-            )}
-            <input
-              type="password"
-              placeholder="Password"
-              className="input-field animation a4"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            {error && password === "" && (
-              <p
-                style={{
-                  lineHeight: "16px",
-                  marginTop: "4px",
-                  color: "red",
-                  fontSize: "12px",
-                }}
-              >
-                Password is required
-              </p>
-            )}
-            {login && (
-              <p className="animation a5">
-                <a href="#" style={{ color: "#171717" }}>
-                  Forgot password?
-                </a>
-              </p>
-            )}
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <button
-                className="animation a6"
-                style={
-                  password === "" && email === ""
-                    ? { background: "#ffd66d", color: "#171717", cursor:"pointer" }
-                    : { background: "#07e9a1", cursor:"pointer" }
-                }
-                onClick={(e) => {
-                  handleSubmit(e);
-                }}
-              >
-                {login ? "Log In" : "Register"}
-              </button>
-            </div>
-          </form>
-          <p
-            className="animation a6"
-            style={{
-              color: "#171717",
-              textAlign: "center",
-              marginTop: "12px",
-              textDecoration: "underline",
-              cursor:"pointer"
-            }}
-            onClick={handleSwitch}
-          >
-            {login ? " Join" : " Log In"}
-          </p>
-        </div>
-      </div>
-      <div className="right"></div>
-    </div>
+    <HomeLayout>
+      {loading ? (
+        <Splash />
+      ) : (
+        <section
+          style={{
+            padding: "60px 24px 100px 24px",
+            backgroundColor: "#f3f4f6",
+            height: "auto",
+            width: "100%",
+          }}
+        >
+          <input type="text" placeholder="input email" />
+          <input type="passwrord" placeholder="input password" />
+        </section>
+      )}
+    </HomeLayout>
   );
-}
+};
 
 export default Login;
--- server/index.js
+++ server/index.js
@@ -1,44 +1,15 @@
-import { OpenAI } from 'openai';
-import express from 'express';
-import cors from 'cors';
-
-const app = express()
-const PORT = 3080
-const openai = new OpenAI({
-    apiKey: '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',
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/Users");
+const app = express();
+app.use(express.json());
+app.use(cors());
+//register API
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  console.log(result);
+  res.send("data saved");
 });
-
-app.use(cors())
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-
-app.post('/', async (req, res) => {
-    const { message } = req.body;
-    console.log(message);
-    try {
-        const completion = await openai.completions.create({
-            model: 'text-davinci-003',
-            prompt: `${message}`,
-            max_tokens: 100,
-            temperature: 0.5,
-        });
-        console.log(completion.choices[0].text)
-
-        // Check if completion.data and completion.data.choices are defined
-        if (completion && completion.choices && completion.choices.length > 0) {
-            res.json({
-                message: completion.choices[0].text,
-            });
-        } else {
-            res.status(500).json({ error: 'Unexpected response format from OpenAI API' });
-        }
-    } catch (error) {
-        console.error('Error calling OpenAI API:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-app.listen(PORT, () =>{
-    console.log(`app is listening at ${PORT}`)
-})
-
+app.listen(5000);
